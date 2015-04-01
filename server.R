4e011af765a06ca6199f75f5e275d5a02fd22b7d
David Nusinow <david@david-pc>
2015-04-01 15:51:55 -0400

    Validate the input a bit
    * Check for only one file upload
    * Set a 5MB file size limit
    * Make sure that the header of the file conforms to specifications
    * Make sure that when the file is read in that the columns have sane types

--- server.R
+++ server.R
@@ -4,12 +4,40 @@ require("cRomppass")
 function(input, output) {
     
     input.experiment <- reactive({
-        indf <- input$experimentUploader
+        indf <- unique(input$experimentUploader)
         if(is.null(indf)) {
             return(NULL)
         }
 
-        read.delim(indf[1, "datapath"], stringsAsFactors = FALSE)
+        validate(need(nrow(indf) == 1,
+                      "Only one uploaded file permitted"))
+        validate(need(indf[1, "size"] < 5000000,
+                      "The uploaded file may not be more than 5 megabytes in size"))
+
+        ## Make sure all the headers are there
+        header <- scan(indf[1, "datapath"], what = character(), nlines = 1, sep = "\t", quiet = TRUE)
+        
+        validate(need(all(c("Experiment ID", "Replicate", "Bait", "Prey", "Spectral Count") %in%
+                          header) |
+                      all(c("Experiment.ID", "Replicate", "Bait", "Prey", "Spectral.Count") %in%
+                          header),
+                      "Missing one or more of the required column names in the input"))
+
+        ## Read in the file and validate that the column classes are sane
+        ret <- read.delim(indf[1, "datapath"], stringsAsFactors = FALSE)
+
+        validate(need(is.character(ret$Experiment.ID) | is.numeric(ret$Experiment.ID),
+                      "The Experiment ID column must be a number or character string"))
+        validate(need(is.character(ret$Replicate) | is.integer(ret$Replicate),
+                      "The Replicate column must be an integer or character string"))
+        validate(need(is.character(ret$Bait),
+                      "The Bait column must be a character string"))
+        validate(need(is.character(ret$Prey),
+                      "The Prey column must be a character string"))
+        validate(need(is.integer(ret$Spectral.Count),
+                      "The Spectral Count column must be an integer"))
+
+        return(ret)
     })
 
     input.stats <- reactive({
